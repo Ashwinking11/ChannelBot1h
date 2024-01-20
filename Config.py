db006cbcd485064ebd46dcd955b7e83cd71940cf
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-01-20 16:37:20 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,13 +6,13 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 LOGGER = loguru.logger
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 0))
+        API_ID = int(os.environ.get('API_ID', 7324525))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = os.environ.get('API_HASH', None)
-    OWNER_ID = os.environ.get('OWNER_ID', 6258709129)
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    API_HASH = os.environ.get('API_HASH', d28604398dc13af15dd108bb34a27a54)
+    OWNER_ID = os.environ.get('OWNER_ID', 1966867320)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', 6509653210:AAEap5ed7KpIh-6Ym679jZzNcAhYORvHJ_k)
+    DATABASE_URL = os.environ.get('DATABASE_URL', mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority)
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', None)
